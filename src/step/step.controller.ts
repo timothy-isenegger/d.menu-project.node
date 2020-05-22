ddd558d4f7dc4e11325019aa957c5d768ef9ddb0
Timothy <timmy.isenegger@gmail.com>
2020-05-22 22:53:52 +0200

    [TASK] Update step controller, remove step interface

--- src/step/step.controller.ts
+++ src/step/step.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Post} from '@nestjs/common';
+import {StepService} from "./step.service";
+import {CreateStepDto} from "../dto/create-step.dto";
+import {Step} from "./step.entity";
 
 @Controller('step')
-export class StepController {}
+export class StepController {
+    constructor(private stepService: StepService) {
+    }
+
+    @Post()
+    create(@Body() createStepDto: CreateStepDto): Promise<Step> {
+        return this.stepService.create(createStepDto);
+    }
+
+    @Get()
+    findAll() {
+        return this.stepService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id) : Promise<Step> {
+        return this.stepService.findOne(id);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id): Promise<void> {
+        return this.stepService.remove(id);
+    }
+}
