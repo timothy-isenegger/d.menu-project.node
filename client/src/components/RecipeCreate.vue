1312a24c372a1ab9b006bfcd558f61a930d8cb04
Timothy <timmy.isenegger@gmail.com>
2020-11-27 12:40:14 +0100

    [TASK] Add instructions and ingredients to creating form

--- client/src/components/RecipeCreate.vue
+++ client/src/components/RecipeCreate.vue
@@ -3,7 +3,7 @@
   <section class="recipe-create">
     <h1>Create your new recipe</h1>
     <div class="recipe-create__grid">
-      <form id="formRecipe" class="recipe-create__form">
+      <form id="formRecipe" class="recipe-create__form" v-on:submit.prevent="submitRecipeForm">
         <div class="form-group">
           <label class="form-control-label" for="recipeTitle">Title</label>
           <input v-model="title" class="form-control" placeholder="Give your recipe a catchy title" id="recipeTitle" />
@@ -15,17 +15,44 @@
         <h4>Add instruction step</h4>
         <div class="form-group">
           <label class="form-control-label" for="recipeStepTitle">Step title</label>
-          <input v-model="stepTitle" class="form-control" placeholder="Give your recipe a catchy title" id="recipeStepTitle" />
+          <input v-model="stepTitle" class="form-control" placeholder="What is the main goal of this step?" id="recipeStepTitle" />
         </div>
         <div class="form-group">
           <label class="form-control-label" for="recipeStepDescription">Step description</label>
-          <input v-model="stepDescription" class="form-control" placeholder="Give your recipe a catchy title" id="recipeStepDescription" />
+          <input v-model="stepDescription" class="form-control" placeholder="Describe short, but clearly" id="recipeStepDescription" />
         </div>
-        <button class="btn btn-outline-secondary" @click="addRecipeStep()">Add</button>
+        <span class="btn btn-outline-secondary" v-on:click="addRecipeStep">Add</span>
+        <h4>Add ingredients</h4>
+        <div class="form-group">
+          <label class="form-control-label" for="ingredientAmount">Amount</label>
+          <input v-model="ingredientAmount" class="form-control" placeholder="Please add ml, gr, etc..." id="ingredientAmount" />
+        </div>
+        <div class="form-group">
+          <label class="form-control-label" for="ingredientName">Name</label>
+          <input v-model="ingredientName" class="form-control" placeholder="What is the product called?" id="ingredientName" />
+        </div>
+        <span class="btn btn-outline-secondary" v-on:click="addIngredient">Add</span>
+
+        <button type="submit" class="btn btn-primary">Create</button>
       </form>
       <div class="recipe-create__presentation">
         <h2>{{ title || 'Your title comes here...' }}</h2>
         <p> {{ description || 'Your fancy description' }}</p>
+        <hr>
+        <div class="recipe-create__instruction">
+          <h4 v-if="hasRecipeSteps">Instructions</h4>
+          <div v-for="step in recipeSteps" v-bind:key="step.title">
+            <h5>{{ step.title }}</h5>
+            <p>{{ step.description }}</p>
+          </div>
+        </div>
+        <div class="recipe-create__ingredients">
+          <h4 v-if="hasIngredients">Ingredients</h4>
+          <ul>
+            <li v-for="ingredient in recipeIngredients" v-bind:key="ingredient.name">{{ingredient.amount}} –
+              {{ ingredient.name }}</li>
+          </ul>
+        </div>
       </div>
     </div>
   </section>
@@ -46,19 +73,59 @@
         description: '',
         stepTitle: '',
         stepDescription: '',
+        recipeSteps: [],
+        ingredientAmount: '',
+        ingredientName: '',
+        recipeIngredients: [],
       }
     },
     methods: {
       addRecipeStep() {
-        // TODO - preventDefault -> click on button should not submit form!
-        // TODO - create new array in data called recipeSteps
-        // TODO - get values from step title & description --> add to recipeSteps
-        // TODO - create list and fill with values from recipeSteps
+        if (this.stepTitle !== '' && this.stepDescription !== '') {
+          const newStep = {
+            title: this.stepTitle,
+            description: this.stepDescription
+          }
 
+          try {
+            this.recipeSteps.push(newStep);
+            this.stepTitle = '';
+            this.stepDescription = '';
+          } catch (error) {
+            // eslint-disable-next-line no-console
+            console.error(error);
+          }
+
+        } else {
+          window.alert('please fill out title and description');
+        }
+      },
+      addIngredient() {
+        if (this.ingredientAmount !== '' && this.ingredientName !== '') {
+          try {
+            this.recipeIngredients.push({
+              amount: this.ingredientAmount,
+              name: this.ingredientName
+            });
+          } catch (error) {
+            // eslint-disable-next-line no-console
+            console.error(error);
+          }
+        } else {
+          window.alert('Please fill out amount and name');
+        }
+      },
+      submitRecipeForm() {
+        // TODO - Save new recipe
       }
     },
     computed: {
-
+      hasRecipeSteps() {
+        return this.recipeSteps.length > 0;
+      },
+      hasIngredients() {
+        return this.recipeIngredients.length > 0;
+      }
     }
 }
 
